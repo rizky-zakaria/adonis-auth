27e6f640f02f05a74fc39ae1e377aef49b2ebf62
Rizky Zakaria <rizky.zakaria78@gmail.com>
2023-12-18 10:31:13 +0700

    --allow-empty

--- start/app.js
+++ start/app.js
@@ -18,7 +18,9 @@ const providers = [
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/shield/providers/ShieldProvider',
   '@adonisjs/session/providers/SessionProvider',
-  '@adonisjs/auth/providers/AuthProvider'
+  '@adonisjs/auth/providers/AuthProvider',
+  '@adonisjs/validator/providers/ValidatorProvider'
+
 ]
 
 /*
@@ -58,4 +60,9 @@ const aliases = {}
 */
 const commands = []
 
-module.exports = { providers, aceProviders, aliases, commands }
+module.exports = {
+  providers,
+  aceProviders,
+  aliases,
+  commands
+}
